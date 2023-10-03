11f76fa9ef6c4a61f9ddccf76d922416eb7c52ec
Adrian Castañeda <acastaneda@urbano.com.pe>
2023-10-03 10:10:58 -0500

    add

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -129,7 +129,7 @@
       </q-page>
     </q-page-container>
 
-    <q-footer v-if="loggedIn" class="bg-black text-white">
+    <q-footer v-if="!isAuthRoute" class="bg-black text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
@@ -147,51 +147,6 @@ import { useRoute } from "vue-router";
 import { useAuthStore } from "stores/auth";
 import { useUtilityStore } from "stores/utility";
 import { storeToRefs } from "pinia";
-
-const linksList = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
-  },
-];
 
 export default defineComponent({
   name: "MainLayout",
